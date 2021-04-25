eaf7de73fff2ec2b971805475a532147b1fe8dab
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-04-25 19:12:11 +0100

    index finalizado

---
+++ src/index.ts
@@ -0,0 +1,144 @@
+import {User} from './Users/users';
+import * as yargs from 'yargs';
+import * as chalk from 'chalk';
+
+
+yargs.command( {
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    username: {
+      describe: 'User',
+      demandOption: true,
+      type: 'string',
+    },
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+    color: {
+      describe: 'Note color',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.username === 'string' && typeof argv.title === 'string' && typeof argv.body === 'string' && typeof argv.color === 'string') {
+      if (argv.color == 'red' || argv.color == 'blue' || argv.color == 'yellow' || argv.color == 'green') {
+        const user = new User(argv.username);
+        user.addNote(argv.title, argv.body, argv.color);
+      } else {
+        console.error(chalk.red('The color asigned its incorrect!'));
+      }
+    }
+  },
+});
+
+
+yargs.command( {
+  command: 'remove',
+  describe: 'Remove an existing note',
+  builder: {
+    username: {
+      describe: 'User',
+      demandOption: true,
+      type: 'string',
+    },
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.username === 'string' && typeof argv.title === 'string') {
+      const user = new User(argv.username);
+      user.removeNote(argv.title);
+    }
+  },
+});
+
+
+yargs.command( {
+  command: 'modify',
+  describe: 'Modify an existing note',
+  builder: {
+    username: {
+      describe: 'User',
+      demandOption: true,
+      type: 'string',
+    },
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    newParam: {
+      describe: 'Parameter wants to change',
+      demandOption: true,
+      type: 'string',
+    },
+    valParam: {
+      describe: 'New parameter content',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.username === 'string' && typeof argv.title === 'string' && typeof argv.newParam === 'string' && typeof argv.valParam === 'string') {
+      const user = new User(argv.username);
+      user.modifyNote(argv.title, argv.newParam, argv.valParam);
+    }
+  },
+});
+
+
+yargs.command( {
+  command: 'read',
+  describe: 'Read a note',
+  builder: {
+    username: {
+      describe: 'User',
+      demandOption: true,
+      type: 'string',
+    },
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.username === 'string' && typeof argv.title === 'string') {
+      const user = new User(argv.username);
+      user.readNote(argv.title);
+    }
+  },
+});
+
+
+yargs.command( {
+  command: 'list',
+  describe: 'List user`s notes',
+  builder: {
+    username: {
+      describe: 'User',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    if (typeof argv.username === 'string') {
+      const user = new User(argv.username);
+      user.listNotes();
+    }
+  },
+});
+
+yargs.argv;
